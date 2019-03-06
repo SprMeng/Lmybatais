66cd39b0e01f6313ea9302dce0c58bfe84aaeae7
zhongfangmeng <supermengz@163.com>
2019-03-06 15:12:20 +0800

    映射文件_select_记录封装

--- mybatis_03_mapper/src/main/java/com/mybatis/dao/EmployeeMapper.java
+++ mybatis_03_mapper/src/main/java/com/mybatis/dao/EmployeeMapper.java
@@ -12,7 +12,9 @@ public interface EmployeeMapper {
 
 	public Employee getById(int id);
 	
-	@MapKey("last_name")
+	//多条记录封装一个map：Map<Integer,Employee>:键是这条记录的主键，值是记录封装后的javaBean
+		//@MapKey:告诉mybatis封装这个map的时候使用哪个属性作为map的key
+	@MapKey("lastName")
 	public Map<String, Employee> getEmpsByLastNameLikeReturnMap(String lastName);
 	
 	public Map<String, Object> getEmpByIdReturnMap(Integer id);
